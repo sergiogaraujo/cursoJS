a628a72bf9d9daecc7b0dbd59b83e0f4413e05a3
Sérgio Araújo <sergiogaraujo@gmail.com>
2025-06-15 16:54:03 -0300

    atualização

--- exercicios/aula16ex/script.js
+++ exercicios/aula16ex/script.js
@@ -4,7 +4,11 @@ let res = document.querySelector('div#res');
 let valores = [];
 
 function isNumero(n) {
-    return Number(n) >= 1 && Number(n) <= 100;
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function inLista(n, l) {
@@ -17,10 +21,20 @@ function adicionar() {
         let item = document.createElement('option');
         item.text = `Valor ${num.value} adicionado.`;
         lista.appendChild(item);
-        res.innerHTML = '';
+        
     } else {
         window.alert('Valor inválido ou já encontrado na lista.');
     }
     num.value = '';
     num.focus();
+}
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+    }
 }
