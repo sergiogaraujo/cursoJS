4caefc3853239dea8c9b245b41185aba2b564579
Sérgio Araújo <sergiogaraujo@gmail.com>
2025-06-15 15:05:39 -0300

    atualizei

---
+++ exercicios/aula16/funcao003.js
@@ -0,0 +1,4 @@
+let v = function (X) {
+    return X*2;
+}
+console.log(v(5)); // Exibe 10
---
+++ exercicios/aula16/funcao004.js
@@ -0,0 +1,12 @@
+//TRADICIONAL
+function fatorial(n) {
+    let fat = 1;
+    for (let c = n; c > 1; c--) {
+        fat *= c;
+    }   
+    return fat; 
+}
+
+console.log(fatorial(5)); // Exibe 120
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
